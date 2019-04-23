5d88993cc478d0605a4cd6ea935531fd955e6df5
unknown <victor.ebs0198@gmail.com>
2019-04-23 17:49:20 +0300

    version 1.0

--- FirstProject/urls.py
+++ FirstProject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pol/', include('polls.urls')),
+    path('999/', include('polls.urls')),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,105 +2,161 @@ from django.http import HttpResponse
 import requests
 from bs4 import BeautifulSoup
 
-def getTitle(titleString):
-    brand = "Volkswagen"
-    model = "Passat"
-    if (brand in titleString) and (model in titleString):
-        print("--"+titleString+"--")
-        return True
-    else:
-        return False
 
-def getPrice(price):
-    priceMultiplier = 1;
-    rawPrice = price
-    if "€" in rawPrice:
-        rawPrice = rawPrice.replace("€", "")
-        priceMultiplier = 20.14
-    if "MDL" in rawPrice:
-        rawPrice = rawPrice.replace("MDL", "")
-    rawPrice = rawPrice.replace(" ", "")
-    Price = rawPrice[:len(rawPrice) - 1]
-    if Price == '':
-        return
-    print("--" + str(int(Price)) + "--")
-    priceInMDL = float(Price) * priceMultiplier
-    return priceInMDL
-
-def processItems(items):
-    title = ""
-    year = 0
-    priceInMDL = 0
-
-    for itemDIV in items.find_all('div'):
-        if str(itemDIV.get('class')) == '[\'ads-list-table-title-wrapper\']':
-            title = getTitle(str(itemDIV.a.string))
-            if not title:
-                return
-
-
-    for yearTDpriceTD in items.find_all('td'):
-        if str(yearTDpriceTD.get('class')) == '[\'ads-list-table-col-3\', \'feature-19\']':
-            rawYear = str(yearTDpriceTD.string).replace(" ", "")
-            year=int(rawYear)
-            print("--"+str(int(year))+"--")
-        if str(yearTDpriceTD.get('class')) == '[\'ads-list-table-price\', \'feature-2\']':
-            price=str(yearTDpriceTD.string)
-            price = getPrice(price)
-            if not price:
-                return
-
-
-    return (year, price)
-
-
-
+# item means car ;)
 
 def index(request):
-    outputresponse = ""
     itemsList = []
-    startYear = 2006
-    endYear = 2010
-    for page in range(1,6):
-        getParams = {'page': str(page)}
-        r = requests.get('https://999.md/ro/list/transport/cars?applied=1&eo=20&view_type=short&r_6_2_unit=eur&ef=260&ef=6&ef=1&ef=7&o_1_21_20=117&r_6_2_to=&r_7_19_from=2006&o_260_1=776&r_7_19_to=2010&r_6_2_from=', data=getParams)
+    startYear = 2005
+    endYear = 2017
+    page_nr = 1
+    pages = 1
+
+    # loop through pages
+    while page_nr <= pages:
+
+        # set variable parameters
+        getParams = {'r_7_19_from': str(startYear), 'r_7_19_to': str(endYear), 'page': str(page_nr)}
+        r = requests.get(
+            'https://999.md/ro/list/transport/cars?applied=1&eo=20&view_type=short&r_6_2_unit=eur&ef=260&ef=6&ef=1&ef=7&hide_duplicates=yes&o_1_21_20=117&r_6_2_to=&r_6_2_from=',
+            data=getParams)
         strFormat = str(r.text)
 
         soup = BeautifulSoup(strFormat, 'html.parser')
 
+        # refresh last page's number, from current page's navigation buttons
+        for navs in soup.find_all('nav'):
+            for page in navs.find_all('li'):
+                if is_number(str(page.a.string)):
+                    pages = int(page.a.string)
+
+        # search for container with results
         for container in soup.find_all('div'):
-
-            if str(container.get('data-view-type'))=="short": # got container
-
-                for items in container.find_all('tr'):
-
-                    titleYearPrice = processItems(items) #get title, year, price of one item
-
-
-
+            # if got page's container with items
+            if (str(container.get('data-view-type')) == "short") and (
+                    str(container.get('class')) == '[\'items__list__container\']'):
+                for item in container.find_all('tr'):
+                    titleYearPrice = processItem(item)  # get title, year and price of one item
+                    # if got valid data, append in list
                     if titleYearPrice:
-                        outputresponse += str(titleYearPrice) + "<br>"
                         itemsList.append(list(titleYearPrice))
-                        print(titleYearPrice)
+        page_nr += 1
 
-    groupedByYears=[]
-    for year in range(startYear, endYear+1):
-        groupedByYears.append([0,0])
-    print("---making groups---")
-    for year in range(startYear, endYear+1):
-        for itemList in itemsList:
-            print(itemList)
-            if itemList[0] == year:
-                print("yes")
-                groupedByYears[year-startYear][0]+=1
-                groupedByYears[year - startYear][1]+=itemList[1]
-    print("---groups by one----")
-    for oneGroup in groupedByYears:
-        print(oneGroup)
+    groupedByYears = []
 
     for year in range(startYear, endYear + 1):
-        print(str(year) + " -> "+ str(round(groupedByYears[year-startYear][1]/groupedByYears[year-startYear][0],2)))
+        groupedByYears.append([0, 0, 0, 500000, 0])
+        groupedByYears[year - startYear][0] = year
+        for itemList in itemsList:
+            if itemList[0] == year:
+                # set cars number
+                groupedByYears[year - startYear][1] += 1
+                # set average price per year
+                groupedByYears[year - startYear][2] += round(itemList[1])
+                # update min price
+                if groupedByYears[year - startYear][3] > itemList[1]:
+                    groupedByYears[year - startYear][3] = round(itemList[1])
+                # update max price
+                if groupedByYears[year - startYear][4] < itemList[1]:
+                    groupedByYears[year - startYear][4] = round(itemList[1])
 
-    return HttpResponse(outputresponse)
+    response = makeHTMLResponse(groupedByYears)
+
+    return HttpResponse(response)
 
 
+def confirmTitle(titleString):
+    brand = "Volkswagen"
+    model = "Passat"
+    if (brand in titleString) and (model in titleString):
+        return True
+    else:
+        return False
+
+def getPrice(rawPrice):
+    priceMultiplier = 1;
+
+    if "€" in rawPrice:
+        rawPrice = rawPrice.replace("€", "")
+        priceMultiplier = 20.14
+
+    if "MDL" in rawPrice:
+        rawPrice = rawPrice.replace("MDL", "")
+
+    rawPrice = rawPrice.replace(" ", "")
+    Price = rawPrice.rstrip()
+
+    # check for empty strings
+    if not Price:
+        return
+
+    priceInMDL = float(Price) * priceMultiplier
+
+    return priceInMDL
+
+
+def processItem(item):
+    year = 0
+
+    for itemDIV in item.find_all('div'):
+        if str(itemDIV.get('class')) == '[\'ads-list-table-title-wrapper\']':
+            title = confirmTitle(str(itemDIV.a.string))
+            if not title:
+                return
+
+    for yearTDpriceTD in item.find_all('td'):
+        if str(yearTDpriceTD.get('class')) == '[\'ads-list-table-col-3\', \'feature-19\']':
+            rawYear = str(yearTDpriceTD.string).replace(" ", "")
+            if is_number(rawYear):
+                year = int(rawYear)
+            else:
+                return
+
+        if str(yearTDpriceTD.get('class')) == '[\'ads-list-table-price\', \'feature-2\']':
+            price = str(yearTDpriceTD.string)
+            price = getPrice(price)
+            if not price:
+                return
+
+    return (year, price)
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+
+def makeHTMLResponse(groupedByYears):
+    response = """<html>
+            <head>
+                <style>
+                    th{
+                        width: 120px;
+                    }
+                </style>
+            </head>
+              <body>
+                <center>
+                    <table border=1>
+                      <tr>
+                        <th>Year</th>
+                        <th>Cars number</th>
+                        <th>Average price</th>
+                        <th>Lowest price</th>
+                        <th>Highest price</th>
+                      </tr>"""
+    for oneGroup in groupedByYears:
+        response += "<tr><td>" + str(oneGroup[0]) + "</td><td>" + str(oneGroup[1]) + "</td><td>" + str(
+            "{:,.2f}".format(round(oneGroup[2] / oneGroup[1]))) + "&nbspMDL</td><td>" + str(
+            "{:,.2f}".format(oneGroup[3])) + "&nbspMDL</td><td>" + str(
+            "{:,.2f}".format(oneGroup[4])) + "&nbspMDL</tr>"
+    response += """</tr>
+                        </table>
+                    <center>
+                  </body>
+                </html>"""
+
+    return response
